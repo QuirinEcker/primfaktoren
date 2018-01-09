40419fbe7b506ba529e9345df0cd33ecbd66ce7e
QuirinEcker <quirin.ecker@bajupa.com>
2018-01-09 10:21:19 +0100

    deleting commnent

--- src/main/java/Ecker_Primfaktroren.java
+++ src/main/java/Ecker_Primfaktroren.java
@@ -5,33 +5,20 @@ public class Ecker_Primfaktroren {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int inputNumber = 100;
+        int inputNumber;
+        int outputNumber;
         int primfactor;
-        boolean loop = true;
 
-        System.out.println("Ganze Zahl: ");
-        inputNumber =scanner.nextInt();
-
-        /*while (inputNumber > 1){
-            if ((inputNumber % 2 )== 0){
-                inputNumber /= 2;
-                System.out.print(" 2 * ");
-            } else if ((inputNumber % 3 == 0)){
-                inputNumber /= 3;
-                System.out.println(" 3 * ");
-            } else if ((inputNumber % 5) == 0){
-                inputNumber /= 5;
-                System.out.print(" 5 *");
-            }
-        }
-        System.out.println(" = " + inputNumber);
-        */
+        System.out.print("Ganze Zahl: ");
+        inputNumber = scanner.nextInt();
+        outputNumber = inputNumber;
 
         for (primfactor = 2; inputNumber > 1; primfactor++){
             while ((inputNumber % primfactor) == 0) {
                 inputNumber = inputNumber / primfactor;
-                System.out.println(" * " + primfactor + " ");
+                System.out.print(" * " + primfactor + " ");
             }
         }
+        System.out.print(" = " + outputNumber);
     }
 }
